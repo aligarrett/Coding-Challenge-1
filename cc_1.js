123db39070b85e1e41256afd2d0af8d5502a3aa7
aligarrett <aliyah.garrett29@gmail.com>
2024-09-01 20:01:24 -0400

    Task 1 - Employee Information

---
+++ cc_1.js
@@ -0,0 +1,12 @@
+// Task 1: Variables and Data Types
+
+let employeeName = "Ged Deg";
+const employeeID = 123;
+var isActive = true;
+
+console.log(employeeName, typeof(employeeName));
+console.log(employeeID, typeof(employeeID));
+console.log(isActive, typeof(isActive));
+
+
+
