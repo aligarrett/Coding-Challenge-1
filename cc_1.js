e11b2aa105852ad01bc6628a295ababf979d940f
aligarrett <aliyah.garrett29@gmail.com>
2024-09-01 21:07:29 -0400

    Task 3 - Financial Calculations

--- cc_1.js
+++ cc_1.js
@@ -20,3 +20,21 @@ const productDetails = {
 }
 
 console.log(products, productDetails);
+
+
+// Task 3: Assignment Operators
+let accountBalance = 7292000;
+accountBalance += 5000;
+console.log("After adding 5000, the account balance is: "+ accountBalance);
+
+accountBalance -= 10000;
+console.log("After subtracting 10000, the account balance is: "+ accountBalance);
+
+accountBalance *= 2000;
+console.log("After multiplying by 2000, the account balance is:"+ accountBalance);
+
+accountBalance /= 5500;
+console.log("After dividing 5500, the account balance is:"+ accountBalance);
+
+accountBalance %= 1500;
+console.log("The remainder when the balance is divided by 1500 is:"+ accountBalance);
